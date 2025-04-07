515f055fd402f5473a2e2c7aef37e6fac02d1e2a
Jordan-M2 <153721319+Jordan-M2@users.noreply.github.com>
2025-04-07 12:37:02 +0100

    Update Deploy-Agent.ps1

--- nable/Deploy-Agent.ps1
+++ nable/Deploy-Agent.ps1
@@ -1,20 +1,20 @@
 function Deploy-Agent {
 	param(
 		[Parameter(Mandatory)]
-		[string]$CompanyID
+		[string]$SiteID
 	)
  
 	if (-Not(Test-Path C:\ITadmin)) { 
 		New-Item "C:\ITadmin" -ItemType Directory | Out-Null
 	}
  
- 	irm "https://github.com/Sphere-12-Limited/Public/raw/refs/heads/main/nable/$CompanyID.EXE" -OutFile "C:\ITadmin\$CompanyID.exe"
+ 	irm "https://github.com/Sphere-12-Limited/Public/raw/refs/heads/main/nable/$SiteID.EXE" -OutFile "C:\ITadmin\$SiteID.exe"
   
 	$CheckInstalled = Get-WmiObject -Class Win32_Product | Where {$_.Name -like "*MSP Core Agent*"}
 	if ($CheckInstalled) {
 		Write-Output "N-Able already installed."
 	} else {
 		Write-Output "Installing N-Able."
-		Start-Process -FilePath "C:\ITadmin\$CompanyID.exe"
+		Start-Process -FilePath "C:\ITadmin\$SiteID.exe"
 	}
 }
