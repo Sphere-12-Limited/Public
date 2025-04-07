7081c491d65ff39867b7da50fa629931e92750a6
Jordan-M2 <153721319+Jordan-M2@users.noreply.github.com>
2025-04-07 11:48:07 +0100

    Update Deploy-Agent.ps1

--- nable/Deploy-Agent.ps1
+++ nable/Deploy-Agent.ps1
@@ -3,9 +3,13 @@ function Deploy-Agent {
 		[Parameter(Mandatory)]
 		[string]$CompanyID
 	)
+ 
 	if (-Not(Test-Path C:\ITadmin)) { 
 		New-Item "C:\ITadmin" -ItemType Directory | Out-Null
 	}
+ 
+ 	irm "https://raw.githubusercontent.com/Sphere-12-Limited/Public/refs/heads/main/nable/$CompanyID.exe" -OutFile "C:\ITadmin\$CompanyID.exe"
+  
 	$CheckInstalled = Get-WmiObject -Class Win32_Product | Where {$_.Name -like "*MSP Core Agent*"}
 	if ($CheckInstalled) {
 		Write-Output "N-Able already installed."
