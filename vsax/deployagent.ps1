61025a34ad5e2adf9b5400f9a0191181b400eced
s12-jordan <jordan.rare@sphere12.com>
2025-07-14 11:56:30 +1200

    Update deployagent.ps1

--- vsax/deployagent.ps1
+++ vsax/deployagent.ps1
@@ -1,1 +1,13 @@
-New-Item -Path "C:\S12" -ItemType "Directory"
+function Deploy-VSAX {
+
+  param(
+    [Parameter(Mandatory)]
+    [string]$VSAXURL
+  )
+
+  if (-Not(Test-Path "C:\S12")) {
+    New-Item -Path "C:\S12" -ItemType "Directory"
+  }
+
+  Invoke-WebRequest $VSAXURL -OutFile "C:\S12\agent.msi"
+}
